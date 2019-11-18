c516f1405cb73ef0d8a454f6f3a2d77402cf80e8
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-11-18 20:05:20 +0300

    done

--- book_app/db/schema.rb
+++ book_app/db/schema.rb
@@ -10,6 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_18_161644) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "number_of_books"
+    t.datetime "author_birthdate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
